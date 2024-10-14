3064f79747ab1e24d658e3ca8784d3e62091924e
Shubhkarmanss <shubhkarmansabharwal@gmail.com>
2024-10-14 11:33:18 +0530

    First year Project

---
+++ Any.py
@@ -0,0 +1,15 @@
+import time
+
+# Record the start time
+start_time = time.time()
+
+# Your code here
+# ...
+
+# Record the end time
+end_time = time.time()
+
+# Calculate the execution time
+execution_time = end_time - start_time
+
+print("Execution time:", execution_time, "seconds")
